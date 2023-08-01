8f3084bb54f35fa808f21a3b5cc0dd8b99e6846a
Volodymyr Vydrin <vova.xnz@gmail.com>
2023-08-01 17:37:51 +0300

    Update generate_using_coco.sh

--- generate_using_coco.sh
+++ generate_using_coco.sh
@@ -4,11 +4,8 @@ python custom_inpainting/generate.py \
 --config_path configs/stable-diffusion/v2-inpainting-inference.yaml \
 --weights_path /media/data/vv/tasks/2023_02_14_stable_diffusion_weights/weights/512-inpainting-ema.ckpt \
 --generation_limit 100000 \
---base_prompt "small head asian man refugee in the underground looks back, amateur photo, assembly line, Stock Footage" \
+--base_prompt "asian man head" \
 --logs_file_path /media/data/vv/tasks/2022_12_05_debug_stable_diffusion2/head.log \
 --context_bbox_size 160 \
 --coco_ann_path /media/data/vv/tasks/2023_05_11_inpainting/heads_coco.json \
 --coco_bbox_padding 20 
-
-# --base_prompt "small head asian man at the garbage dump at night in fog, amateur photo, looks back, BAD, POOR quality" \f
-# --base_prompt "small head asian man criminal theft is caught surfeillance camera footage, captive refugee in the underground, breaking news, looks back, amateur photo" \
